eac9ba3e00ec2c50e52a79d802d91c0c35b47a35
JereGG <64101683+JeremiasGoncebat@users.noreply.github.com>
2023-06-24 12:03:40 -0300

    Add files via upload
    First commit

---
+++ TerceraPreEntrega/TerceraPreEntrega/view.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+
+def inicio(request):
+    pass
+
+def alumnos(request):
+    pass
+
+def cursos(request):
+    pass
+def profesores(request):
+    pass
+def entregas(request):
+    pass
